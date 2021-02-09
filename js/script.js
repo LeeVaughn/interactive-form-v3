6670db5e4207e41a2e5c3b7c58850c25dc63e615
LeeVaughn <dlvprogramming@gmail.com>
2021-02-09 15:46:09 -0800

    Add comments planning out project

---
+++ js/script.js
@@ -0,0 +1,42 @@
+// set the focus state on the Name field when the app loads
+
+// hide the Job Role text field
+
+// show Credit Card as the selected method of payment when the app loads
+
+// create an event listener for the Job Role dropdown
+  // show the text field when "other" is selected, otherwise hide it
+
+// disable the Color element
+
+// create an event listener for the Design dropdown
+  // hide all colors
+  // when JS Puns is selected, show JS Pun colors
+  // when I <3 JS is selected, show I <3 JS colors
+
+// create an event listener for the Activities section
+  // update the cost when an activity is checked or unchecked
+  // prevent the user from selecting overlapping activities (exceeds)
+  // add focus to the activities when the user tabs through the form
+
+// show Credit Card as the selected method of payment when the app loads
+
+// create an event listener for the Payment section
+  // hide all payment info
+  // if CC is selected, shows CC info
+  // if PayPal is selected, show PayPal info
+  // if Bitcoin is selected, show Bitcoin info
+  // add at least one real time validation (exceeds)
+  // add at least one conditional error message (exceeds)
+
+// add validations
+  // name cannot be blank
+  // email must contain valid address
+  // at least one activity must be selected
+  // if credit card is the selected method of payment
+    // card number must be a number between 13 and 16 digits
+    // zip code must be a five digit number
+    // cvv must be a three digit number
+
+// create an event listener for the Register button
+  // when there are validation errors the form should be prevented from submitting
